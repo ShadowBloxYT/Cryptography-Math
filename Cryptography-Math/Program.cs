87a6d9813cde96073b0970ee50f79c6293e2457c
Myron Lai <mindstormev89@outlook.com>
2019-11-17 11:49:06 -0800

    Added start

---
+++ Cryptography-Math/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cryptography_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
