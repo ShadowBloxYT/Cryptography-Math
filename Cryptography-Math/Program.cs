669485e16f85251546695049b94436da33e72081
Myron Lai <mindstormev89@outlook.com>
2019-12-15 11:44:06 -0800

    Added more cryptography functions

--- Cryptography-Math/Program.cs
+++ Cryptography-Math/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 
 namespace Cryptography_Math
 {
     class Program
     {
-        public static void factor(string yn)
+        public static void factor()
         {
             Console.WriteLine("enter a number");
             BigInteger respond = BigInteger.Parse(Console.ReadLine());
@@ -17,59 +18,231 @@ namespace Cryptography_Math
                     Console.WriteLine($"{i}");
                 }
             }
-            Console.WriteLine("wanna continue? type (Y) yes or (N) no");
-            yn = Console.ReadLine();
+            Console.WriteLine("DONE");
+        }
 
-            if (yn.ToLower() == "y")
-            {
-                factor(yn);
-            }
+        public static void GCF()
+        {
+            Console.WriteLine("Enter a number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            int secondNumber = int.Parse(Console.ReadLine());
 
-            else if (yn.ToLower() == "n")
+            int whatNum = 0;
+
+            if (firstNumber > secondNumber)
             {
-                Console.WriteLine("Ok thanks for using the factor machine! See you next time!");
+                whatNum = secondNumber;
             }
 
             else
             {
-                Console.WriteLine("That is not a valid answer please try again");
-            
-                yn = Console.ReadLine();
+                whatNum = firstNumber;
+            }
 
-                if (yn.ToLower() == "y")
+            for (int i = 1; i < whatNum + 1; i++)
+            {
+                if (firstNumber % i == 0 && secondNumber % i == 0)
                 {
-                    factor(yn);
+                    Console.WriteLine(i);
                 }
+            }
+            Console.WriteLine("DONE");
+        }
 
-                else if (yn.ToLower() == "n")
+        public static void Relatively_Prime()
+        {
+            Console.WriteLine("Enter a number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int whatNum = 0;
+
+            List<int> factors = new List<int>();
+
+            if (firstNumber > secondNumber)
+            {
+                whatNum = secondNumber;
+            }
+
+            else
+            {
+                whatNum = firstNumber;
+            }
+
+            for (int i = 1; i < whatNum + 1; i++)
+            {
+                if (firstNumber % i == 0 && secondNumber % i == 0)
                 {
-                    Console.WriteLine("Ok thanks for using the factor machine! See you next time!");
+                    factors.Add(i);
+                }
+            }
+
+            if (factors.Count == 1)
+            {
+                Console.WriteLine("True");
+            }
+
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.WriteLine("DONE");
+        }
+
+        public static void isPrime()
+        {
+            List<BigInteger> factors = new List<BigInteger>();
+
+            Console.WriteLine("enter a number");
+            BigInteger respond = BigInteger.Parse(Console.ReadLine());
+            for (BigInteger i = 1; i < respond + 1; i++)
+            {
+                if (respond % i == 0)
+                {
+                    factors.Add(i);
+                }
+            }
+
+            if (factors.Count == 2)
+            {
+                Console.WriteLine("True");
+            }
+
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.WriteLine("DONE");
+        }
+
+        public static void uniqueRelativelyPrime()
+        {
+            Console.WriteLine("Calculating...");
+
+            for (int i = 0; i < 101; i++)
+            {
+                for (int j = i + 1; j < 101; j++)
+                {
+                    int firstNumber = i;
+
+                    int secondNumber = j;
+
+                    int whatNum = 0;
+
+                    List<int> factors = new List<int>();
+
+                    if (firstNumber > secondNumber)
+                    {
+                        whatNum = secondNumber;
+                    }
+
+                    else
+                    {
+                        whatNum = firstNumber;
+                    }
+
+                    for (int k = 1; k < whatNum + 1; k++)
+                    {
+                        if (firstNumber % k == 0 && secondNumber % k == 0)
+                        {
+                            factors.Add(k);
+                        }
+                    }
+
+                    if (factors.Count == 1)
+                    {
+                        Console.WriteLine($"Number 1: {firstNumber}, Number 2: {secondNumber}");
+                    }
+                }
+            }
+
+            Console.WriteLine("DONE");
+        }
+
+        public static void multiplicativeInverse()
+        {
+            Console.WriteLine("Enter a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int x = 0;
+
+            int result = 0;
+
+            while (true)
+            {
+                if ((x * (num1)) % num2 == 1)
+                {
+                    result = x;
+                    break;
                 }
 
                 else
                 {
-                    Console.WriteLine("That is not a valid answer please try again");
-
-                    yn = Console.ReadLine();
-
-                    if (yn.ToLower() == "y")
-                    {
-                        factor(yn);
-                    }
-
-                    else if (yn.ToLower() == "n")
-                    {
-                        Console.WriteLine("Ok thanks for using the factor machine! See you next time!");
-                    }
+                    x++;
                 }
+
+                Console.WriteLine($"X: {x}, Number 1: {num1}, Number 2: {num2}, Result: {(x * (num1)) % num2}");
             }
+
+            Console.WriteLine($"Result number: {result}");
         }
+
+        public static BigInteger Factorial(int num)
+        {
+            if (num < 1)
+            {
+                return 1;
+            }
+
+
+            return num * Factorial(num -1);
+        }
+
+
         static void Main(string[] args)
         {
-            string yn = "";
+            Console.WriteLine("Enter a string");
+            string input = Console.ReadLine();
 
-            factor(yn);
-            
+            int length = input.Length;
+
+            char[] letters = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                letters[i] = input[i];
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                char extra = letters[i];
+                if (i >= letters.Length)
+                {
+                    char extra2 = letters[0];
+                    letters[0] = letters[i];
+                    letters[i] = extra2;
+                }
+                else
+                {
+                    letters[i] = letters[i + 1];
+                    letters[i + 1] = extra;
+                }
+            }
+
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"{i}: {letters[i]}");
+            }
+
+            Console.WriteLine(Factorial(length));
+
+            Console.ReadKey();
         }
     }
 }
